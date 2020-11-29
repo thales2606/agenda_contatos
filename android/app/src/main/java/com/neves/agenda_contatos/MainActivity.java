77e0b9d530803c513dfa7da9381ba49b341f01ab
Thales Neves <thales_2606@hotmail.com>
2020-11-28 22:59:42 -0300

    Initial commit

---
+++ android/app/src/main/java/com/neves/agenda_contatos/MainActivity.java
@@ -0,0 +1,6 @@
+package com.neves.agenda_contatos;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/ui/contact_page.dart
@@ -0,0 +1,147 @@
+import 'dart:io';
+
+import 'package:agenda_contatos/helpers/contact_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ContactPage extends StatefulWidget {
+  final Contact contact;
+  ContactPage({this.contact});
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+
+  final _nameFocus = FocusNode();
+
+  bool _userEdited = false;
+  Contact _editedContact;
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.red,
+            title: Text(_editedContact.name ?? "Novo Contato"),
+            centerTitle: true,
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              if (_editedContact.name != null &&
+                  _editedContact.name.isNotEmpty) {
+                Navigator.pop(context, _editedContact);
+              } else {
+                FocusScope.of(context).requestFocus(_nameFocus);
+              }
+            },
+            child: Icon(Icons.save),
+            backgroundColor: Colors.red,
+          ),
+          body: SingleChildScrollView(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                GestureDetector(
+                  child: Container(
+                    width: 140,
+                    height: 140,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image: _editedContact.img != null &&
+                                    _editedContact.img.isNotEmpty
+                                ? FileImage(File(_editedContact.img))
+                                : AssetImage("images/person.jpeg"))),
+                  ),
+                  onTap: (){
+                    ImagePicker.platform.pickImage(source: ImageSource.camera)
+                    .then((file) {
+                      if(file == null) return;
+                      setState(() {
+                        _editedContact.img = file.path;
+                        _userEdited = true;
+                      });
+                    });
+                  },
+                ),
+                TextField(
+                  controller: _nameController,
+                  focusNode: _nameFocus,
+                  decoration: InputDecoration(labelText: "Nome"),
+                  onChanged: (text) {
+                    _userEdited = true;
+                    setState(() {
+                      _editedContact.name = text;
+                    });
+                  },
+                ),
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(labelText: "Email"),
+                  onChanged: (text) {
+                    _userEdited = true;
+                    _editedContact.email = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                TextField(
+                  controller: _phoneController,
+                  decoration: InputDecoration(labelText: "Phone"),
+                  onChanged: (text) {
+                    _userEdited = true;
+                    _editedContact.phone = text;
+                  },
+                  keyboardType: TextInputType.phone,
+                )
+              ],
+            ),
+          ),
+        ),
+        onWillPop: _requestPop);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.contact == null) {
+      _editedContact = Contact();
+    } else
+      _editedContact = Contact.fromMap(widget.contact.toMap());
+    _nameController.text = _editedContact.name;
+    _emailController.text = _editedContact.email;
+    _phoneController.text = _editedContact.phone;
+  }
+
+  Future<bool> _requestPop() {
+    if (_userEdited) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text("Descartar Alterações?"),
+              content: Text("Se sair as alterações serão perdidas."),
+              actions: [
+                FlatButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("Cancelar")),
+                FlatButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.pop(context);
+                    },
+                    child: Text("Sim"))
+              ],
+            );
+          });
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
+  }
+}
